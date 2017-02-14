ecc7f0f99dca13eeffbf24dc67b5b6632fbb3d7b
Marcel Williams <marcelwilliams503@gmail.com>
2017-02-13 20:21:05 -0800

    made some changes to make my score get log into the console

--- game.js
+++ game.js
@@ -1,48 +1,48 @@
+var count = 0;
 
 
-  prompt("Are you ready to play!");
-  default:blue;
-  confirm("Lets get this party started!")
-  var age = prompt("How old am I?");
-  var score = 0;
-  if (age < 24) {
-    console.log("Meh, maybe we can get to know each other a bit more!");
-  } else {
-    console.log("I am 24 years old!");
-  }
+
+confirm("Lets get this party started!")
+ var age = prompt("How old am I?");
+ if (age < 24) {
+   alert("Meh, maybe we can get to know each other a bit more!");
+   count++;
+ } else {
+   alert("I am 24 years old!");
+ }
 
 
-  //This is were my second question begins.
-  alert("Next question")
-  var team = prompt("What's my favorite NFL team located in New Orleans Lousiana!?");
-  var team = "Saints".toUpperCase
-  if (team === "saints".toUpperCase) {
-    console.log("WHO DAT! WHO DAT! WHO DAT SAY GONNA BEAT THE Saints!");
+ //This is were my second question begins.
+ alert("Next question")
+ var userTeam = prompt("What's my favorite NFL team located in New Orleans Lousiana!?");
+ if ( userTeam === "saints".toUpperCase) {
+   alert("WHO DAT! WHO DAT! WHO DAT SAY GONNA BEAT THE Saints!");
+   count++;
 
-  } else {
-    console.log("Wrong answer but know you know who the best team in the NFl is now!");
-  }
+ } else {
+   alert("Wrong answer but know you know who the best team in the NFl is now!");
+ }
 
-  //This is were my third question begins.
-  alert("We are almost done, just a couple more questions")
-  var color = prompt("Whats my favorite color?");
-  var color = "blue".toLowerCase
-  if (color = "blue".toLowerCase) {
-    console.log("This is the true blue answer!");
-  } else {
-    console.log("Its okay you didnt guess blue I love all colors!");
-  }
-  alert("Favorite color is blue")
-
-  var number = prompt("Whats my favorite number in the twentys")
-  var answer = alert
-  if(number === 25) {
-    console.log("You got the corrrect answer!")
+ //This is were my third question begins.
+ console.log("We are almost done, just a couple more questions")
+ var questionThree = prompt("Whats my favorite color?");
+ var color = "blue".toLowerCase
+ if (questionThree = "blue".toLowerCase) {
+   alert("This is the true blue answer!");
+   count++;
+ } else {
+   alert("Its okay you didnt guess blue I love all colors!");
+ }
+ alert("Favorite color is blue")
+ var number = 25;
+ var userInput = prompt("Whats my favorite number in the twentys")
+ if(userInput === number) {
+   console.log("You got the corrrect answer!")
+   count++;
 }
-else if(number < 24){
-    console.log("not that low!")
-}
-   else {
-    alert("Wrong answer try again")
+else(userInput > 0)
+   console.log("not that low!");
 
-}
+
+console.log("You got "+ count + " correct answers");
+confirm("You got "+ count + " correct answers");
